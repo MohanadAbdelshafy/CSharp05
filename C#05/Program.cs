1d2d59e441c68ab2418d17b8eac30e991198ad74
mohandabdelshafy443-eng <mohandabdelshafy443@gmail.com>
2026-09-08 21:50:54 +0300

    Question 1

--- C#05/Program.cs
+++ C#05/Program.cs
@@ -1,10 +1,20 @@
 ﻿namespace C_05
 {
+    #region Question 1 book class
+    //class Book
+    //{
+    //    private string password = "secret";
+    //}
+    #endregion
     internal class Program
     {
         static void Main(string[] args)
         {
-           
+            #region Question 1 object creating
+            //Book b = new Book();
+            //Console.WriteLine(b.password);
+            ////compile time error,because the password feild is protected (can't access outside the class) 
+            #endregion
         }
     }
 }
